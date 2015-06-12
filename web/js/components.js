2db00f741454ad71f1b7a4bb6e9762aca20fe1f7
fain182 <fain182@gmail.com>
2015-06-12 16:27:45 +0200

    Added list of organizations in admin

--- web/js/components.js
+++ web/js/components.js
@@ -1,27 +1,15 @@
 var AddOrganizationForm = React.createClass({
-    getInitialState: function() {
-        return {message: ''};
-    },
     handleSubmit: function(e) {
         e.preventDefault();
-        var name = React.findDOMNode(this.refs.name).value.trim();
-        if (!name) {
-            return;
-        }
-        var component = this;
-        $.post( "/admin/organizations", {'name': name})
-            .done(function() {
-                component.setState({message: 'Organization added'});
-                React.findDOMNode(component.refs.name).value = '';
-            }).fail(function() {
-                component.setState({message: 'Error'});
-            });
+        var name = React.findDOMNode(this.refs.name).value;
+        this.props.onFormSubmit(name);
+        React.findDOMNode(this.refs.name).value = '';
     },
     render: function() {
         return (
             <form className="commentForm" onSubmit={this.handleSubmit}>
                 <h1>Add an Organization</h1>
-                <h3 ref="message">{this.state.message} </h3>
+                <h3 ref="message">{this.props.message} </h3>
                 Organization name: <input ref="name"/>
                 <br />
                 <input type="submit" value="Add" className="btn btn-primary" />
@@ -30,7 +18,75 @@ var AddOrganizationForm = React.createClass({
     }
 });
 
+var Organization = React.createClass({
+    render: function() {
+        return (
+            <div className="well">{this.props.name}</div>
+        );
+    }
+});
+
+var OrganizationList = React.createClass({
+    render: function() {
+        var nodes = this.props.data.map(function (organization) {
+            return (
+                <Organization name={organization.name}/>
+            );
+        });
+        return (
+            <div>
+                <h1>Organizations</h1>
+                {nodes}
+            </div>
+        );
+    }
+});
+
+var OrganizationAdmin = React.createClass({
+    getInitialState: function() {
+        return {message: '', organizations: []};
+    },
+    componentDidMount: function() {
+        this.fetchOrganizations();
+    },
+    fetchOrganizations: function () {
+        $.ajax({
+            url: '/admin/organizations',
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({message: this.state.message, organizations: data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(status, err.toString());
+            }.bind(this)
+        });
+    },
+    handleSubmit: function(name) {
+        if (!name) {
+            return;
+        }
+        var component = this;
+        $.post( "/admin/organizations", {'name': name})
+            .done(function() {
+                component.setState({message: 'Organization added'});
+                component.fetchOrganizations();
+            }).fail(function() {
+                component.setState({message: 'Error'});
+            });
+    },
+    render: function () {
+        return (
+            <span>
+                <AddOrganizationForm onFormSubmit={this.handleSubmit} message={this.state.message}/>
+                <OrganizationList data={this.state.organizations} />
+            </span>
+        )
+    }
+});
+
+
 React.render(
-    <AddOrganizationForm />,
+    <OrganizationAdmin />,
     document.getElementById('content')
 );
