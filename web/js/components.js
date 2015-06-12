b0b2c0344b23aaba1931fa8cdef73d6df0322bff
fain182 <fain182@gmail.com>
2015-06-12 14:56:06 +0200

    form is submitted by react, base bootstrap layout

--- web/js/components.js
+++ web/js/components.js
@@ -1,8 +1,30 @@
 var AddOrganizationForm = React.createClass({
+    getInitialState: function() {
+        return {message: ''};
+    },
+    handleSubmit: function(e) {
+        e.preventDefault();
+        var name = React.findDOMNode(this.refs.name).value.trim();
+        if (!name) {
+            return;
+        }
+        var component = this;
+        $.post( "/admin/organizations", {'name': name})
+            .done(function() {
+                component.setState({message: 'Organization added'});
+                React.findDOMNode(component.refs.name).value = '';
+            }).fail(function() {
+                component.setState({message: 'Error'});
+            });
+    },
     render: function() {
         return (
-            <form action="organizations" method="post">
-                Organization name: <input name="name"/>
+            <form className="commentForm" onSubmit={this.handleSubmit}>
+                <h1>Add an Organization</h1>
+                <h3 ref="message">{this.state.message} </h3>
+                Organization name: <input ref="name"/>
+                <br />
+                <input type="submit" value="Add" className="btn btn-primary" />
             </form>
         );
     }
