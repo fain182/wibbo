f63d1079db66105c99603e83e5ebe5a06320e80b
fain182 <fain182@gmail.com>
2015-06-13 12:36:34 +0200

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
+git push heroku master
 # "run:detach" because "run" has problem on fastweb:
 # http://stackoverflow.com/questions/8582860/heroku-run-console-get-timeout-awaiting-process
 heroku run:detached './vendor/bin/phinx migrate -e production'
-
-sleep 60 # There is no simple way to know when migrations are completed, for now this is good enough.
-git push heroku master
