30f6bada753a9491fdbce59974e256585561ceb8
fain182 <fain182@gmail.com>
2015-06-17 18:49:50 +0200

    Tested average time for incidents

--- test/Wibbo/FunctionalTest/IncidentTest.php
+++ test/Wibbo/FunctionalTest/IncidentTest.php
@@ -35,27 +35,51 @@ class IncidentTest extends WibboTestCase {
 
     public function testWithIncidentsClosed()
     {
-        $this->requestAuthenticated(
-          'POST',
-          '/admin/organizations/'.$this->organizationId.'/incidents',
-          ['description' => 'fake problem 2']
-        );
+        $this->addIncidentClosed(5);
 
-        $this->client->request('GET', '/organizations/'.$this->organizationId.'/incidents/now');
-        $activeIncidents = json_decode($this->client->getResponse()->getContent());
-        $incidentId = $activeIncidents[0]->id;
-
-
-        $this->requestAuthenticated(
-          'PATCH',
-          '/admin/organizations/'.$this->organizationId.'/incidents/'.$incidentId,
-          ['finish' => '2015-06-17T16:02:46.070Z']
-        );
         $this->client->request('GET', '/organizations/'.$this->organizationId.'/incidents/now');
         $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $activeIncidents = json_decode($this->client->getResponse()->getContent());
         $this->assertEquals(0, count($activeIncidents));
     }
 
+
+    public function testAverageDurationWithoutIncident()
+    {
+        $this->client->request('GET', '/organizations/'.$this->organizationId.'/stats');
+        $stats = json_decode($this->client->getResponse()->getContent());
+        $this->assertEquals(15, $stats->averageIncidentDuration);
+    }
+
+    public function testAverageDurationWithIncident()
+    {
+        $this->addIncidentClosed(8);
+        $this->addIncidentClosed(12);
+
+        $this->client->request('GET', '/organizations/'.$this->organizationId.'/stats');
+        $stats = json_decode($this->client->getResponse()->getContent());
+        $this->assertEquals(10, $stats->averageIncidentDuration);
+    }
+
+    protected function addIncidentClosed($inNMinutesFromNow)
+    {
+        $this->requestAuthenticated(
+          'POST',
+          '/admin/organizations/' . $this->organizationId . '/incidents',
+          ['description' => 'fake problem 2']
+        );
+
+        $this->client->request('GET', '/organizations/' . $this->organizationId . '/incidents/now');
+        $activeIncidentsBeforeClosing = json_decode($this->client->getResponse()->getContent());
+        $incidentId = $activeIncidentsBeforeClosing[0]->id;
+
+        $finishTimeInUtc = new \DateTime(null, new \DateTimeZone('UTC'));
+        $finishTimeInUtc->add(new \DateInterval("PT".$inNMinutesFromNow."M"));
+        $this->requestAuthenticated(
+          'PATCH',
+          '/admin/organizations/' . $this->organizationId . '/incidents/' . $incidentId,
+          ['finish' => $finishTimeInUtc->format(\DateTime::ISO8601)]
+        );
+    }
 
 }
